72ba094630cbb62c7f582a6f288fdbd4cd460746
vas-alina <vasalina93@gmail.com>
2024-04-27 11:26:26 +0300

    создание отдельного jsx файла с новым объектом-текущий год

---
+++ my-app/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+const year = new Date().getFullYear();
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload 2.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p className="App-year">{year}</p>
+			</header>
+		</div>
+	);
+};
